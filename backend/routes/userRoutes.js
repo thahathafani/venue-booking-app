9e22a48ee80b4fc85975f2cc8f74b375cd014d50
thahathafani <thahafazzin@gmail.com>
2024-08-09 17:17:22 +0530

    token api completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -68,3 +68,27 @@ router.post("/login", async (req, res) => {
 
 
 // validate token api
+
+router.get("/me", async (req, res) => {
+    const token = req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+        return res.status(401).json({ message: "No token, authorization denied"});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id).select("-password");
+
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        res.json(user);
+    } catch (error) {
+        res.status(401).json({ message: "Token is not found" });
+    }
+});
+
+export default router;
