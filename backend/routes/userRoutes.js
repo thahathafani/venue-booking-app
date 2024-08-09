c549adf964da16b2ee9852c6f521ebc8cc6e79a9
thahathafani <thahafazzin@gmail.com>
2024-08-09 16:50:24 +0530

    registration api completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,3 +11,29 @@ const router = express.Router();
 
 // Register api
 
+router.post("/register", async (req, res) => {
+    const { name, email, password, isHost } = req.body;
+
+    try{
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({ mesaage: "User already exists" });
+        }
+
+        const user = new User({ name, email, password, isHost });
+        await user.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "30d",
+        });
+
+        res.status(201).json({ token });
+    } catch (error){
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+    
+});
+
