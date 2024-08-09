f1e0e71bb36edbf0f921ef9c150b1b5b852936dd
thahathafani <thahafazzin@gmail.com>
2024-08-09 17:35:39 +0530

    auth middleware completed

--- backend/controllers/authMiddleware.js
+++ backend/controllers/authMiddleware.js
@@ -1,2 +1,34 @@
 // middleware
 
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.header.authorization.startWith("Bearer")) {
+        
+        try{
+            token = req.header.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select("-password");
+
+            next();
+
+        } catch (error) {
+            res.status(401).json({
+                message: " Not authorized, token failed"
+            });
+        }
+    }
+
+    if (!token) {
+        res.status(401).json({ message: "Not authorized, no token" });
+    }
+};
+
+export default protect;
