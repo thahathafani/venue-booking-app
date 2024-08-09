d3238cf61321e0a931c037d5429039f3087e8c7c
thahathafani <thahafazzin@gmail.com>
2024-08-09 17:41:13 +0530

    user profile completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -93,6 +93,18 @@ router.get("/me", async (req, res) => {
 
 // user profile
 
-
+router.get("/profile", protect, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Server error" });
+    }
+  });
 
 export default router;
