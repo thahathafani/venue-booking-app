575343cc759e2b006135904a12461d9a5d2257e3
thahathafani <thahafazzin@gmail.com>
2024-08-09 17:12:35 +0530

    login api completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -38,3 +38,30 @@ router.post("/register", async (req, res) => {
 });
 
 // Login api 
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid email or password"});
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "30",
+        });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ message: "server error" });
+    }
+
+});
