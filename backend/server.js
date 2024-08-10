5b535752a419f98b5756b167442bd61f29047027
thahathafani <thahafazzin@gmail.com>
2024-08-10 10:34:54 +0530

    basic server completed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
 // basic server api
+
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import hostRoutes from "./routes/hostRoutes.js";
+
+
+dotenv.config();
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/hosts", hostRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
